e1cb2c2611fc9b5c04244401e7181a64f335787e
Curtis Ling <zebulunssword@gmail.com>
2020-05-19 23:21:57 -0700

    2020-05-19

--- ToastDemo1.ps1
+++ ToastDemo1.ps1
@@ -3,16 +3,16 @@
 # https://www.youtube.com/watch?v=qAnKMu2v3Bk
 # https://smsagent.blog/2018/06/15/using-windows-10-toast-notifications-with-configmgr-application-deployments/
 
-$Text1 = New-BTText -Text 'Windows Updates'
-$Text2 = New-BTText -Text 'There are updates available for your workstation.  Please install them as soon as possible.'
+$Text1 = New-BTText -Text 'Hello There!'
+$Text2 = New-BTText -Text 'This is a snippet of information that you can give to the user. Please make this brief.'
 
-$Image = New-BTImage -Source 'C:\Temp\DeploymentGeek.png' -AppLogoOverride -Crop None
+$Image = New-BTImage -Source "$PSScriptRoot\DeploymentGeek.ico" -AppLogoOverride -Crop None
 
 $Binding1 = New-BTBinding -Children $Text1, $Text2 -AppLogoOverride $Image
 
 $Visual1 = New-BTVisual -BindingGeneric $Binding1
 
-$Button1 = New-BTButton -Content 'Install' -Arguments 'https://deploymentgeek.com' -ActivationType Protocol
+$Button1 = New-BTButton -Content 'Go!' -Arguments 'http://deploymentgeek.com' -ActivationType Protocol
 $Button2 = New-BTButton -Dismiss
 $Button3 = New-BTButton -Snooze
 
@@ -20,6 +20,7 @@ $Action1 = New-BTAction -Buttons $Button1, $Button2, $Button3
 
 $Content1 = New-BTContent -Visual $Visual1 -Duration Long -Actions $Action1
 
-Submit-BTNotification -Content $Content1 -AppId 'DeploymentGeek.com!DeploymentGeek'
+Submit-BTNotification -Content $Content1
+
 
 
