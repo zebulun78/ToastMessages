5fe625b7d868e7aa7fd4d87eb6c6b06e1f7bd62c
Curtis Ling <zebulunssword@gmail.com>
2020-05-20 00:29:52 -0700

    2020-05-20

--- ToastDemo1.ps1
+++ ToastDemo1.ps1
@@ -6,17 +6,27 @@
 $Text1 = New-BTText -Text 'Hello There!'
 $Text2 = New-BTText -Text 'This is a snippet of information that you can give to the user. Please make this brief.'
 
-$Image = New-BTImage -Source "https://github.com/zebulun78/ToastMessages/raw/master/DeploymentGeek.ico" -AppLogoOverride -Crop None
+$5Min = New-BTSelectionBoxItem -Id 5 -Content '5 minutes'
+$10Min = New-BTSelectionBoxItem -Id 10 -Content '10 minutes'
+$1Hour = New-BTSelectionBoxItem -Id 60 -Content '1 hour'
+$4Hour = New-BTSelectionBoxItem -Id 240 -Content '4 hours'
+$1Day = New-BTSelectionBoxItem -Id 1440 -Content '1 day'
 
-$Binding1 = New-BTBinding -Children $Text1, $Text2 -AppLogoOverride $Image
+$SnoozeOptions = $5Min, $10Min, $1Hour, $4Hour, $1Day
+$SnoozeSelectionBox = New-BTInput -Id 'SnoozeTime' -DefaultSelectionBoxItemId 10 -Items $SnoozeOptions -Title "Snooze for:"
+
+$HeroImage = New-BTImage -Source "$PSScriptRoot\Images\HeroImage1.png" -HeroImage
+$LogoImage = New-BTImage -Source "$PSScriptRoot\Images\DeploymentGeek.png" -AppLogoOverride -Crop Circle
+
+$Binding1 = New-BTBinding -Children $Text1,$Text2 -AppLogoOverride $LogoImage -HeroImage $HeroImage
 
 $Visual1 = New-BTVisual -BindingGeneric $Binding1
 
 $Button1 = New-BTButton -Content 'Go!' -Arguments 'http://deploymentgeek.com' -ActivationType Protocol
-$Button2 = New-BTButton -Dismiss
-$Button3 = New-BTButton -Snooze
+$Button2 = New-BTButton -Snooze -Id 'SnoozeTime'
+$Button3 = New-BTButton -Dismiss
 
-$Action1 = New-BTAction -Buttons $Button1, $Button2, $Button3
+$Action1 = New-BTAction -Buttons $Button1,$Button2,$Button3 -Inputs $SnoozeSelectionBox
 
 $Content1 = New-BTContent -Visual $Visual1 -Duration Long -Actions $Action1
 
